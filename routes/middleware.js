983b1460d01f06ba99ecd138d9fa45a55696f082
tahirabbas001 <tahir.abbas@riksof.com>
2024-07-21 03:24:11 +0500

    add netlify deployment

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const auth = async (req, res, next) => {
     const authorization = req.headers.authorization;
